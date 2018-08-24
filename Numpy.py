0a3ef8de397b120e5a830d31d6795fa55ff221e5
HuskyInTheNorth <32262115+HuskyInTheNorth@users.noreply.github.com>
2018-08-24 12:56:46 +0800

    Update Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -19,6 +19,6 @@ array(深拷贝)
 asarray(浅拷贝)
 
 #Numpy创建随机数组——均匀分布
-np.random.rand(10.10)
+np.random.rand(10.10)              #创建指定形状的数组(范围在0至1之间)
 np.random.uniform(0.100)           #创建指定范围内的一个数
 np.random.randint(0.100)           #创建指定范围内的一个整数
